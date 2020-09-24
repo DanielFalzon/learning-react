be3c483849bd2ea50ab77578c8a329505ba1d806
Daniel Falzon <daniel.falzon@avantechsoftware.com>
2020-09-24 17:48:02 +0200

    Tailwind implementation and using react spring.

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+function Header(){
+    return(
+        <header className="border-b flex justify-between items-center p-3">
+            <span className="font-bold">
+                App Name
+            </span>
+            <Navigation />
+        </header>
+
+    )
+}
+
+export default Header;
